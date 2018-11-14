322bd86d01021fb206afee682ce22de4a29392fd
pratik2799 <pithore.pratik@gmail.com>
2018-11-14 16:48:03 +0530

    Revert "msm8956-common: Go back to passthrough camera hidl hal"
    This reverts commit 2208951fee539ae83e641773b61e4bbb20daa8a5.

--- msm8956.mk
+++ msm8956.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
