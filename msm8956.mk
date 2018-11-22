f740ff7cd8efb9db049b25c8368d850916f2b2a3
sayan7848 <sayan7848@gmail.com>
2018-11-22 08:23:34 +0530

    msm8956: Compile XiaomiParts
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -424,3 +424,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
